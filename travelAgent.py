33c37f99b192c03faae353461f68234471fef6f8
Lorenzo <lorenzo.matheo@hotmail.com>
2025-01-24 12:26:24 -0300

    Agrupando todos os métodos na pesquisa na web na função researchAgent()

--- travelAgent.py
+++ travelAgent.py
@@ -4,25 +4,30 @@ from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
 from langchain import hub
 
+
+
+
 # Inicializa o modelo de linguagem
 llm = ChatOpenAI(model="gpt-3.5-turbo")
 
 # Configura a API do Wikipedia
 wiki_api_wrapper = WikipediaAPIWrapper()
 
-# Define a ferramenta com o wrapper configurado
-tools = [WikipediaQueryRun(api_wrapper=wiki_api_wrapper)]
-
-prompt = hub.pull("hwchase17/react")
-
-agent = create_react_agent(llm,tools, prompt)
-
-agent_executor = AgentExecutor(agent=agent,tools=tools,prompt=prompt,verbose=True)
-
-# Exemplo de uso do agente
 query = """
 Vou viajar para França em julho de 2025. Quero que faça um roteiro de viagens para mim com os eventos que irão ocorrer na data da viagem 
 e com o preço de passagem de São Paulo pra Londres
 """
 
-agent_executor.invoke({"input": query})
+def researchAgent(query, llm):
+    tools = [WikipediaQueryRun(api_wrapper=wiki_api_wrapper)]
+    prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm,tools, prompt)
+    agent_executor = AgentExecutor(agent=agent,tools=tools,prompt=prompt,verbose=True)
+    webContext = agent_executor.invoke({"input": query})
+    return webContext['output']
+
+print(researchAgent(query, llm))
+
+
+
+
